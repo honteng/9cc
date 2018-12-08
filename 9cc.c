253b73c1d8cb8e939b7c652ad17770336678df0a
Naoki Takano <honten@tinkermode.com>
2018-12-08 20:13:14 +0000

    supported term gen

--- 9cc.c
+++ 9cc.c
@@ -8,6 +8,9 @@ enum {
   TK_EOF,
 };
 
+enum {
+  ND_NUM = 256,
+};
 
 typedef struct {
   int ty;
@@ -15,7 +18,23 @@ typedef struct {
   char *input;
 } Token;
 
+int pos = 0;
 Token tokens[100];
+
+typedef struct Node {
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
+Node *term();
+
+void error(int i) {
+  fprintf(stderr, "unknown char %s", tokens[i].input);
+  exit(1);
+}
+
 
 void tokenize(char *p) {
   int i = 0;
@@ -25,7 +44,8 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' ||
+        *p == '(' || *p == ')' ) {
       tokens[i].ty = *p;
       tokens[i].input = p;
       p++;
@@ -41,7 +61,7 @@ void tokenize(char *p) {
       continue;
     }
 
-    fprintf(stderr, "unknown char %s", p);
+    fprintf(stderr, "unknown char %s in tokenizer", p);
     exit(1);
   }
 
@@ -49,11 +69,94 @@ void tokenize(char *p) {
   tokens[i].input = p;
 }
 
-void error(int i) {
-  fprintf(stderr, "unknown char %s", tokens[i].input);
-  exit(1);
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
 }
 
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+
+Node *mul() {
+  Node *lhs = term();
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+  return lhs;
+}
+
+Node *expr() {
+  Node *lhs = mul();
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+Node *term() {
+  if (tokens[pos].ty == TK_NUM) {
+    return new_node_num(tokens[pos++].val);
+  }
+  if (tokens[pos].ty == '(') {
+    pos++;
+    Node *node = expr();
+    if (tokens[pos].ty != ')') {
+      error(pos);
+    }
+    pos++;
+    return node;
+  }
+  error(pos);
+}
+
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch(node->ty) {
+    case '+':
+      printf("  add rax, rdi\n");
+      break;
+   case '-':
+      printf("  sub rax, rdi\n");
+      break;
+   case '*':
+      printf("  mul rdi\n");
+      break;
+   case '/':
+      printf("  mov rdx, 0\n");
+      printf("  div rdi\n");
+      break;
+  }
+  printf("  push rax\n");
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -62,40 +165,14 @@ int main(int argc, char **argv) {
   }
 
   tokenize(argv[1]);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  if (tokens[0].ty != TK_NUM) {
-    error(0);
-  }
-
-  printf("  mov rax, %d\n", tokens[0].val);
-
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM) {
-        error(i);
-      }
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-    error(i);
-  }
-
+  gen(node);
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
